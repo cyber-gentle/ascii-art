9bea9ab64df93d6fd1a264f25999596165297282
cyber-gentle <info.abdavid@gmail.com>
2026-04-29 10:06:23 +0100

    fix: renamed variable name

--- main.go
+++ main.go
@@ -7,39 +7,39 @@ import (
 )
 
 var (
-	input_string       string
-	first_banner_name  string
-	second_banner_name string
-	third_banner_name  string
+	inputtedText       string
+	firstBannerName  string
+	secondBannerName string
+	thirdBannerName  string
 )
 
 func main() {
 
 	switch len(os.Args) {
 	case 2:
-		input_string = os.Args[1]
-		first_banner_name = "standard"
+		inputtedText = os.Args[1]
+		firstBannerName = "standard"
 
 	case 3:
-		input_string = os.Args[1]
-		first_banner_name = gen2.LowerCase(os.Args[2])
+		inputtedText = os.Args[1]
+		firstBannerName = gen2.LowerCase(os.Args[2])
 
 	// case 4:
-	// 	input_string = os.Args[1]
-	// 	first_banner_name = gen2.LowerCase(os.Args[2])
-	// 	second_banner_name = gen2.LowerCase(os.Args[3])
+	// 	inputtedText = os.Args[1]
+	// 	firstBannerName = gen2.LowerCase(os.Args[2])
+	// 	secondBannerName = gen2.LowerCase(os.Args[3])
 
 	// case 5:
-	// 	input_string = os.Args[1]
-	// 	first_banner_name = gen2.LowerCase(os.Args[2])
-	// 	second_banner_name = gen2.LowerCase(os.Args[3])
-	// 	third_banner_name = gen2.LowerCase(os.Args[4])
+	// 	inputtedText = os.Args[1]
+	// 	firstBannerName = gen2.LowerCase(os.Args[2])
+	// 	secondBannerName = gen2.LowerCase(os.Args[3])
+	// 	thirdBannerName = gen2.LowerCase(os.Args[4])
 
 	default:
 		fmt.Println("\n Usage: go run . <words to process to design.>  <file name of the design type.> \n E.g. go run . Hello  shadow.txt \n Or   go run . Hello")
 		return
 	}
 
-	content := loadBanner("banners/" + first_banner_name + ".txt")
+	content := loadBanner("banners/" + firstBannerName + ".txt")
 
 }
