bf2cb48c5a05d1fb64dae76536b1402bb6643d9b
cyber-gentle <info.abdavid@gmail.com>
2026-04-29 09:58:38 +0100

    fix

--- helpers.go
+++ helpers.go
@@ -2,17 +2,7 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
-
-// func fileReader
-func fileReader(fileName string) ([]byte, error) {
-	content, err := os.ReadFile(fileName)
-	if err != nil {
-		return nil, err
-	}
-	return content, nil
-}
 
 // func logErr that prints error to the standard output.
 func logErr(err error) {
