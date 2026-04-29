c5f1b61b474900dfedf8dbc2a2ea1918783022ed
cyber-gentle <info.abdavid@gmail.com>
2026-04-29 11:21:02 +0100

    feat: add func getCharRow to that locates the index of the char and fetch it's row

--- render.go
+++ render.go
@@ -1,1 +1,12 @@
 package main
+
+func getCharRows(ch rune, bannerLines []string) []string {
+	index := int(ch) - 32
+	start := index * 9
+	return bannerLines[start+1 : start+9]
+
+}
+
+func renderLine() {
+
+}
