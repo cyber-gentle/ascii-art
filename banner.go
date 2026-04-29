c034e66705883bf3936e121825b350ed4887383a
cyber-gentle <info.abdavid@gmail.com>
2026-04-29 09:57:56 +0100

    feat: add func loadBanner that reads and load banner content

--- banner.go
+++ banner.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"os"
+	"strings"
+)
+
+func loadBanner(filename string) []string {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		logErr(err)
+	}
+
+	data := strings.Split(string(content), "\n")
+	return data
+}
