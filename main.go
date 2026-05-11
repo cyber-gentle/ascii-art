317800c9d42bfdb89c1b95efba0666062f1a1b69
Abraham David <info.abdavid@gmail.com>
2026-05-11 16:20:41 +0100

    add func main and go.mod

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"ascii-art/processor"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var (
+		input      string
+		bannerName string
+		validated  bool
+		err error
+	)
+
+	switch len(os.Args) {
+	case 2:
+		input = os.Args[1]
+		bannerName = "standard.txt"
+
+	case 3:
+		input = os.Args[1]
+		bannerName = os.Args[2]
+	}
+
+	bannerMap, err := processor.LoadBanner(bannerName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	words := processor.SplitInput(input)
+
+	for _, word := range words {
+		validated, err = processor.ValidateInput(word)
+		if err != nil || !validated {
+			fmt.Println(err)
+			return
+		} else {
+			processor.GenerateArt(word, bannerMap)
+		}
+	}
+}
